6d034723ab7da1fb9991b8a6f6add45e3d732a90
Ronak-Lio <84918627+Ronak-Lio@users.noreply.github.com>
2022-03-22 15:01:11 +0530

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,8 @@ import {MoralisProvider} from "react-moralis"
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MoralisProvider
-      appId = {process.env.NEXT_PUBLIC_APP_ID}
-      serverUrl  = {process.env.NEXT_PUBLIC_SERVER_URL}
+      appId = '3L4DzyYLDOjwQ73AhutvOqYOZ49WOe8cJlJgFdnF'
+      serverUrl  = "https://boh2wtpnxvbh.usemoralis.com:2053/server"
     >
       <Component {...pageProps} />
     </MoralisProvider>
